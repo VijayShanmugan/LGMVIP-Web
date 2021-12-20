32de5aa7e4b93467e399dadc6c6b23982704eb9e
VijayShanmugan <77104562+VijayShanmugan@users.noreply.github.com>
2021-12-20 22:53:21 +0530

    Add files via upload

---
+++ Task 2/src/App.js
@@ -0,0 +1,46 @@
+import  React,{useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [click, changeinclick ] = useState(false);
+
+  useEffect(() =>
+  {
+     getdata();   
+  } ,[]);
+
+  const clickevent = () =>
+  {
+    changeinclick(!click);
+  }
+
+  const getdata = async () =>
+  {
+      const response = await fetch("https://reqres.in/api/users?page=1");
+      const content = await response.json();
+      setUsers(content.data);
+  }
+
+  return (
+    <div className="App">
+      <button className="button" onClick={clickevent}>Get Data </button>
+      
+      {click && (
+        <div className="box">
+          <h2>USER DETAILS</h2>
+            
+            {users.map((users) => (
+             <div className="data" key={users.id}>
+                <h4>Name : {users.first_name}{users.last_name}</h4>
+                <img src={users.avatar} alt=""></img>
+                <h5>Email : {users.email}</h5>
+              </div>
+              ))}
+        </div>
+      )}    
+    </div>
+  );
+}
+
+export default App;
